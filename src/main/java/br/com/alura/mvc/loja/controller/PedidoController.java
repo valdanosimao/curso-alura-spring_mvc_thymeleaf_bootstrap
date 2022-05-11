c96ffad70446a0c18d612f274441d5662a64d5fe
Valdano Simão <valdanosimao@gmail.com>
2022-05-10 21:24:21 -0300

    implementando o salvar pedido

--- src/main/java/br/com/alura/mvc/loja/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/loja/controller/PedidoController.java
@@ -2,15 +2,34 @@ package br.com.alura.mvc.loja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.loja.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.loja.model.Pedido;
+import br.com.alura.mvc.loja.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("/pedido")
 public class PedidoController {
 	
+	private final PedidoRepository pedidoRepository;
+	
+	public PedidoController(PedidoRepository pedidoRepository) {
+		this.pedidoRepository = pedidoRepository;
+	}
+
 	@GetMapping("/formulario")
 	public String formulario() {
 		return "formulario";
+	}
+	
+	@PostMapping("/novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
+		return "/formulario";
 	}
 
 }
