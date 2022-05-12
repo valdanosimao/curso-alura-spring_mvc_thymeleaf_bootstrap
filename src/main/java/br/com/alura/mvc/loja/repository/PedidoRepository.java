beb36b04e1bbff882271b685f745293b9cb977d0
Valdano Simão <valdanosimao@gmail.com>
2022-05-12 14:28:26 -0300

    implementando rota de status

--- src/main/java/br/com/alura/mvc/loja/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/loja/repository/PedidoRepository.java
@@ -1,9 +1,16 @@
 package br.com.alura.mvc.loja.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.loja.model.Pedido;
+import br.com.alura.mvc.loja.model.StatusPedido;
 
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 
 }
